4878269e3d743580dc08852a69122a4cae4c2a56
amine <madmanamine2@gmail.com>
2023-10-28 21:30:51 +0100

    added underline to navbar links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,15 +4,19 @@ import { buttonVariants } from "./ui/button";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import SideMenu from "./sideMenu";
+import { useEffect, useState } from "react";
 
 
 const Items = [
   { title: "Apparel", href: "/apparel" },
   { title: "Patterns", href: "/patterns" }]
 export default function Navbar() {
+  const [location, setLocation] = useState("")
+  useEffect(() => {
+    setLocation(window.location.pathname)
+  }, [])
   return (
     <div className="w-screen h-20 flex items-center justify-between">
-
       <Link href="/">
         <Image
           className="ml-5"
@@ -30,8 +34,10 @@ export default function Navbar() {
               className={
                 cn(
                   buttonVariants({ variant: "link" }),
+                  location == item.href ? "underline" : ""
                 )
               }
+              onClick={() => setLocation(item.href)}
             >{item.title}</Link>
           )
         })}
